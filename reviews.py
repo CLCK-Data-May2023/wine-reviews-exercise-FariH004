c376feb70c4ecba4fe9b2e1884e567649684a1f2
Fary Hussain <125521235+FariH004@users.noreply.github.com>
2023-09-03 21:13:58 -0400

    reviews.py

--- reviews.py
+++ reviews.py
@@ -1,20 +1,20 @@
 import pandas as pd
 
 # Read the CSV data
-data = pd.read_csv("data/reviews-per-country.csv")
+data = pd.read_csv('wine-reviews-exercise-FariH004/data/winemag-data-130k-v2.csv.zip')
 
 # Group data by country and calculate summary statistics
 summary = data.groupby('country').agg({
     'points': ['count', 'mean']
-}).reset_index()
+}).reset_index().round(1)
 
 # Rename columns for clarity
-summary.columns = ['country', 'num_reviews', 'avg_points']
+summary.columns = ['country', 'count', 'points']
 
 # Save the summary data to a new CSV file
-summary.to_csv('data/reviews-per-country.csv', index=False)
+summary.to_csv('wine-reviews-exercise-FariH004/data/reviews-per-country.csv', index=False)
 
-print("Summary data saved to 'data/reviews-per-country.csv'")
+
 
 
 
